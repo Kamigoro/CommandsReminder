7f6f98d6573807178235d5a9bc976c3b9725dc7d
Dylan Piette <44063839+Kamigoro@users.noreply.github.com>
2020-11-03 09:56:04 +0100

    Creating DTO's and profiles

---
+++ CommandsReminder/DTO/CommandReadDTO.cs
@@ -0,0 +1,23 @@
+﻿using CommandsReminder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsReminder.DTO
+{
+    public class CommandReadDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Example { get; set; }
+
+        public ICollection<Platform> Platforms { get; set; }
+
+        public List<Parameter> Parameters { get; set; }
+    }
+}
---
+++ CommandsReminder/DTO/PlatformReadDTO.cs
@@ -0,0 +1,17 @@
+﻿using CommandsReminder.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsReminder.DTO
+{
+    public class PlatformReadDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public ICollection<Command> Commands { get; set; }
+    }
+}
